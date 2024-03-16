30e82d90470d0539219310b355506c1beb498767
Molina-Q <quentinmolina1@gmail.com>
2024-03-16 14:49:51 +0100

    the game file

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,64 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from "react-native";
 import nfcManager, { NfcEvents } from "react-native-nfc-manager";
-
-import React from 'react'
+import AndroidPrompt from "./AndroidPrompt";
 
 function Game(props) {
-    async function scanTag() {
-        nfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
-            console.warn('tag found', tag);
+    const [start, setStart] = React.useState(null);
+    const [duration, setDuration] = React.useState(0);
+    const androidPromptRef = React.useRef();
+
+    React.useEffect(() => {
+        let count = 5;
+
+        nfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+            count--;
+            if (Platform.OS === 'android') {
+                androidPromptRef.current.setHintText(`${count}...`);
+            } else {
+                nfcManager.setAlertMessageIOS(`${count}...`);
+            }
+            
+            if (count < 0) {
+                nfcManager.unregisterTagEvent().catch(() => 0);
+                setDuration(new Date().getTime() - start.getTime());
+
+                if(Platform.OS === 'android') {
+                    androidPromptRef.current.setVisible(false);
+                }
+            }
         })
+
+        return () => {
+            nfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+        };
+
+    }, [start])
+    
+
+    async function scanTag() {
         await nfcManager.registerTagEvent();
+        if(Platform.OS === 'android') {
+            androidPromptRef.current.setVisible(true);
+        }
+        setStart(new Date());
+        setDuration(0);
     }
-  return (
-    <View style={styles.wrapper}>
-        <Text>NFC Game</Text>
-    </View>
-  )
+
+    return (
+        <View style={styles.wrapper}>
+            <Text>NFC Game</Text>
+            {duration > 0 && <Text>{duration} ms</Text>} 
+            <TouchableOpacity style={styles.btn} onPress={scanTag}>
+                <Text>START</Text>
+            </TouchableOpacity>
+            <AndroidPrompt ref={androidPromptRef} 
+                onCancelPress={() => {
+                    nfcManager.unregisterTagEvent().catch(() => 1)
+                }} 
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -24,6 +67,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    btn: {
+        margin: 15,
+        padding: 15,
+        borderRadius: 8,
+        backgroundColor: '#ccc',
+    }
 });
 
 export default Game
